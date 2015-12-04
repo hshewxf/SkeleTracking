abfeb7e9116d9e11f95dabddb841c86f560f1672
anag2 <anag2@ncsu.edu>
2015-12-04 02:03:20 -0500

    Initial Commit
    Without co-ordinate shift tracking

---
+++ TrackSkele/glut.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <Windows.h>
+#include <GL/glew.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include <GL/glut.h>
+
+bool init(int argc, char* argv[]);
+void draw();
+void execute();
